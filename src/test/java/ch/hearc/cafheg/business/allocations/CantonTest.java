06e590bc7e31b047cf344ba18a2fd09eab472d44
christopdetraz <christop.detraz@gmail..com>
2024-03-13 15:23:22 +0100

    add 26 canton

--- src/test/java/ch/hearc/cafheg/business/allocations/CantonTest.java
+++ src/test/java/ch/hearc/cafheg/business/allocations/CantonTest.java
@@ -3,16 +3,19 @@ package ch.hearc.cafheg.business.allocations;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class CantonTest {
 
   @Test
+  @DisplayName("fromValue given NE should be NE")
   void fromValue_GivenFR_ShouldBeFR() {
     assertThat(Canton.fromValue("FR")).isEqualTo(Canton.FR);
   }
 
   @Test
+  @DisplayName("fromValue given MM should be null")
   void fromValue_GivenMM_ShouldBeNull() {
     assertThat(Canton.fromValue("MM")).isNull();
   }
