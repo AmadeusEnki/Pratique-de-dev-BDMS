62c8d9345fad8d1971018e66e4e69aa776d7d9d3
AmadeusEnki <milanenmars@gmail.com>
2024-06-07 17:57:30 +0200

    Logger

--- src/main/java/ch/hearc/cafheg/infrastructure/persistance/Migrations.java
+++ src/main/java/ch/hearc/cafheg/infrastructure/persistance/Migrations.java
@@ -1,12 +1,15 @@
 package ch.hearc.cafheg.infrastructure.persistance;
 
 import org.flywaydb.core.Flyway;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Gestion des scripts de migration sur la base de données.
  */
 public class Migrations {
 
+  private static final Logger logger = LoggerFactory.getLogger(Migrations.class);
   private final Database database;
   private final boolean forTest;
 
@@ -17,26 +20,27 @@ public class Migrations {
 
   /**
    * Exécution des migrations
-   * */
+   */
   public void start() {
-    System.out.println("Doing migrations");
+    logger.info("Début de l'exécution des migrations");
 
     String location;
     // Pour les tests, on éxécute que les scripts DDL (création de tables)
     // et pas les scripts d'insertion de données.
-    if(forTest) {
-      location =  "classpath:db/ddl";
+    if (forTest) {
+      location = "classpath:db/ddl";
+      logger.debug("Exécution en mode test, location: {}", location);
     } else {
-      location =  "classpath:db";
+      location = "classpath:db";
+      logger.debug("Exécution en mode production, location: {}", location);
     }
 
     Flyway flyway = Flyway.configure()
-        .dataSource(database.dataSource())
-        .locations(location)
-        .load();
+            .dataSource(database.dataSource())
+            .locations(location)
+            .load();
 
     flyway.migrate();
-    System.out.println("Migrations done");
+    logger.info("Migrations terminées avec succès");
   }
-
 }
